0e7fcdbf74d8d712008393d2cc7958af30e6d6e4
aiman <abduldanmaihaja@gmail.com>
2023-02-06 21:10:03 +0100

    answers for lesson 5

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,25 @@
-console.log('Task Manager App')
+const express = require("express");
+const tasks = require("./routes/tasks");
+const app = express();
+const connectDB = require("./db/connect");
+require("dotenv").config();
+app.use(express.json());
+
+app.get("./hello", (req, res) => {
+  res.send("Task manager app");
+});
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
